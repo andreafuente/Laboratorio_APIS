c57a77cedf2de8e0810a3115328210767015cbcc
Andrea Fuente <andrea.fuente.fdez.a11y@gmail.com>
2026-02-28 20:14:53 +0100

    Test

---
+++ src/motor.spec.ts
@@ -0,0 +1,53 @@
+import {filtradoPersonajes, personajesUrl } from "./motor";
+import { baseUrl } from "./model";
+
+  describe("personajesUrl", () => {
+
+    it("devuelve /personajes cuando no hay nombre", () => {
+      // Given
+      const nombre = "";
+
+      // When
+      const url = personajesUrl(baseUrl, nombre);
+
+      // Then
+      expect(url).toBe("http://localhost:3000/personajes");
+    });
+
+    it("devuelve URL con nombre_like cuando hay filtro", () => {
+      // Given
+      const nombre = "morta";
+
+      // When
+      const url = personajesUrl(baseUrl, nombre);
+
+      // Then
+      expect(url).toBe(
+        "http://localhost:3000/personajes?nombre_like=morta"
+      );
+    });
+  });
+
+  describe("filtradoPersonajes", () => {
+
+    it("devuelve personajes filtrados por nombre", async () => {
+      // Given
+      const nombre = "morta";
+
+      // When
+      const personajes = await filtradoPersonajes(baseUrl, nombre);
+
+      // Then
+      expect(personajes).toEqual([
+        {
+          id: 1,
+          nombre: "Mortadelo",
+          apodo: "Mortadelo",
+          especialidad: "Disfrazarse de cualquier cosa",
+          habilidades: ["Ingenio", "Astucia"],
+          amigo: "Filemón",
+          imagen: "mortadelo.jpg",
+        },
+      ]);
+    });
+  });
